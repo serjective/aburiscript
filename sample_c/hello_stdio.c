0a045fa68b0d0e440499dc8085ebbd5b8ee50004
serjective <git@serjective.org>
2026-03-23 11:31:36 -0400

    update readme to be more descriptive, fix docs linking, and also update test stdio file ("zdravo" is serbo-croatian)

--- sample_c/hello_stdio.c
+++ sample_c/hello_stdio.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    const char *names[] = {"Aburi", "Ghana", "Serbia", "Romania", "Albania", "Compiler"};
-    for (int i = 0; i < 6; ++i) {
+    const char *names[] = {"Aburi", "Ghana", "Serbia", "Croatia", "Romania", "Albania", "Compiler"};
+    for (int i = 0; i < 7; ++i) {
         printf("Maakye! Zdravo! Buna! Pershendetje! Hello from %s\n", names[i]);
     }
 
